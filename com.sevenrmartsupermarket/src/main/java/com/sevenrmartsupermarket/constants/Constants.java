9d129d2c96bbec749b4625c01619e67b05b4d086
HP <HP@LAPTOP-N1JREC8U>
2025-05-30 07:39:29 +0530

    Fourth commit

--- com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/constants/Constants.java
+++ com.sevenrmartsupermarket/src/main/java/com/sevenrmartsupermarket/constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 	
 	public static final long IMPLICIT_WAIT=10; //to declare constant variables
 	public static final String CONFIG_FILE_PATH = System.getProperty("user.dir")+"//src//main//resources//config.properties";
-	public static final String EXCEL_FILE_PATH = System.getProperty("user.dir")+"//src//main//resources//testdata//testdataExcel.xlsx";
+	public static final String EXCEL_FILE_PATH = System.getProperty("user.dir")+"//src//main//resources//testdata//AdminUserTestData.xlsx";
 	public static final String SCREENSHOTS_FILE_PATH = System.getProperty("user.dir")+"//SCREENSHOTS//";
 	public static final String EXTENT_REPORTS_FILE_PATH =  System.getProperty("user.dir")+"//extentreports";
 	//public static final String EXTENT_REPORTS_FILE_PATH= System.getProperty("user.dir") +"//extentreports";
